4a3e28431614e9736345480f8c4a557a5df5502c
doriischeng <dorisc712@gmail.com>
2022-10-08 21:55:13 +0800

    Added the HTML structure of the weather app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
+import React from "react";
 import "./App.css";
 import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-      <Weather />;
+      <Weather />
       <footer>
         This project was coded by{" "}
-        <a href="https://www.itsmedoris.com">
+        <a
+          href="https://www.itsmedoris.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Doris Cheng
         </a>{" "}
         and is{" "}
-        <a href="https://github.com/doriischeng/react-weather-app-final">
+        <a
+          href="https://github.com/doriischeng/react-weather-app-final"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           open-sourced on GitHub
         </a>{" "}
         and{" "}
-        <a href="https://bejewelled-chimera-018133.netlify.app">
+        <a
+          href="https://bejewelled-chimera-018133.netlify.app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           hosted on Netlify
         </a>
       </footer>
--- src/Weather.js
+++ src/Weather.js
@@ -4,7 +4,45 @@ import "./Weather.css";
 export default function Weather() {
   return (
     <div className="Weather">
-      <h1>Weather App</h1>;
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="text"
+              placeholder="Enter a city"
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary"
+            />
+          </div>
+        </div>
+      </form>
+      <h1>Hong Kong</h1>
+      <ul>
+        <li>Saturday 21:40</li>
+        <li>Clear Sky</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/sunny_s_cloudy.png"
+            alt="weather-icon"
+          ></img>
+          26°C
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 5%</li>
+            <li>Humidity: 73%</li>
+            <li>Wind: 19km/h</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
