73283b219a6247eb22e9b01cb5792cd08c2e6bcb
doriischeng <dorisc712@gmail.com>
2022-10-08 21:19:56 +0800

    Moved footer to App.js instead to make it cleaner

--- src/App.js
+++ src/App.js
@@ -5,6 +5,20 @@ export default function App() {
   return (
     <div className="App">
       <Weather />;
+      <footer>
+        This project was coded by{" "}
+        <a href="https://www.itsmedoris.com">
+          Doris Cheng
+        </a>{" "}
+        and is{" "}
+        <a href="https://github.com/doriischeng/react-weather-app-final">
+          open-sourced on GitHub
+        </a>{" "}
+        and{" "}
+        <a href="https://bejewelled-chimera-018133.netlify.app">
+          hosted on Netlify
+        </a>
+      </footer>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -5,20 +5,6 @@ export default function Weather() {
   return (
     <div className="Weather">
       <h1>Weather App</h1>;
-      <footer>
-        This project was coded by{" "}
-        <a href="https://www.itsmedoris.com">
-          Doris Cheng
-        </a>{" "}
-        and is{" "}
-        <a href="https://github.com/doriischeng/react-weather-app-final">
-          open-sourced on GitHub
-        </a>{" "}
-        and{" "}
-        <a href="https://bejewelled-chimera-018133.netlify.app">
-          hosted on Netlify
-        </a>
-      </footer>
     </div>
   );
 }
